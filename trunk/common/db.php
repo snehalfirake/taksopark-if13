f7df89d301f6630e872e1f962bb137073e6715e1
zap@tlu.ee <zap@tlu.ee@e2c1bf8b-2388-1911-6c94-c79eb88be6e8>
2013-11-28 22:16:36 +0000

    some changes to functions.php and db.php

---
+++ trunk/common/db.php
@@ -0,0 +1,5 @@
+<?php
+
+    #   this will be filled with database table creation queries
+
+?>
---
+++ trunk/common/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+    #   this is prettifyed var_dump, use it well young padawans.
+    
+    function preDump($var){
+        echo "<pre>";
+            var_dump($var);
+        echo "</pre>";
+    }
+
+?>
