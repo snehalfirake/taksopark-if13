e96d330bfdbf68b93b2055004ab8c1327def038a
rahnel@tlu.ee <rahnel@tlu.ee@e2c1bf8b-2388-1911-6c94-c79eb88be6e8>
2014-01-07 23:18:29 +0000

    reklaam

--- trunk/taxi2/index.php
+++ trunk/taxi2/index.php
@@ -9,5 +9,7 @@
 			<div id="login">
 				<?php require("login.php"); ?>
 			</div>		
+		<h2> TaxiGO on Eesti esimene taksofirma, mis pakub kaasaegset ja mugavat online-taksotellimisvõimalust! Registreerudes meie teenusega avaneb meie kasutajatele võimalus igal ajal tellida otse internetist omale takso järgi.<h2>
+		<div> Meie kliendid kõnearve lõppedes jala koju jalutama ei pea! </div>
 	</body>
 </html>
