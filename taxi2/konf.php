10b72834706f44a1233799400260c10d8dfc979f
trege <trege@tlu.ee>
2014-01-07 18:09:15 +0000

    konf

---
+++ taxi2/konf.php
@@ -0,0 +1,8 @@
+<?php
+ $baasiaadress="localhost";
+ $baasikasutaja="if13";
+ $baasiparool="ifikad";
+ $baasinimi="if13_egert_k";
+ $yhendus=new mysqli($baasiaadress, $baasikasutaja, $baasiparool, $baasinimi);
+ 
+ //PHP lõpumärki pole vaja, et kogemata midagi välja ei trükitaks
