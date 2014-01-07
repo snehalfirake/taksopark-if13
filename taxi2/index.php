b4925c4a00eee79da78bd6179fdfc8a94c06b141
rnormak <rnormak@tlu.ee>
2014-01-07 18:34:27 +0000

    väiksed parandused 2

--- taxi2/index.php
+++ taxi2/index.php
@@ -4,8 +4,8 @@
   require_once("konf.php"); 
 $yhendus=new mysqli("localhost", "if13", "ifikad", "if13_egert_k");
 if(isSet($_REQUEST["Submit"])){
-    $kask=$yhendus->prepare("INSERT INTO kasutaja(kasutajanimi, paroolir2si, eesnimi, perekonnanimi, email, roll) VALUES (?, PASSWORD(?), ?, ?, ?, ?)");
-    $kask->bind_param("ssssss", $_REQUEST["kasutajanimi"], $_REQUEST["paroolir2si"], $_REQUEST["eesnimi"], $_REQUEST["perekonnanimi"], $_REQUEST["email"], $_REQUEST["roll"]);
+    $kask=$yhendus->prepare("INSERT INTO kasutaja(kasutajanimi, paroolir2si, eesnimi, perekonnanimi, email) VALUES (?, PASSWORD(?), ?, ?, ?)");
+    $kask->bind_param("sssss", $_REQUEST["kasutajanimi"], $_REQUEST["paroolir2si"], $_REQUEST["eesnimi"], $_REQUEST["perekonnanimi"], $_REQUEST["email"]);
     $kask->execute();
 	$yhendus->close();
     header("Location: $_SERVER[PHP_SELF]");
@@ -51,11 +51,6 @@ if(isSet($_REQUEST["Submit"])){
 </tr>
 
 <tr>
-<td><label for="roll">Roll*:</label> </td>
-<td><input id="roll" maxlength="45" name="roll" type="text" /> </td>
-</tr>
-
-<tr>
 <td align="right"><input name="Submit" type="submit" value="Submit" /></td>
 </tr>
  
@@ -65,8 +60,8 @@ if(isSet($_REQUEST["Submit"])){
 <div class="login">
       <h1>Logi kasutajasse</h1>
       <form method="post" action="index.php">
-        <p><input type="text" name="login" value="" placeholder="kasutajanimi"></p>
-        <p><input type="paroolir2si" name="paroolir2si" value="" placeholder="paroolir2si"></p>
+        <p><input type="text" name="login" value="" placeholder="Kasutajanimi"></p>
+        <p><input type="paroolir2si" name="paroolir2si" value="" placeholder="Parool"></p>
         <p class="remember_me">
           <label>
             <input type="checkbox" name="remember_me" id="remember_me">
