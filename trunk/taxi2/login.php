3151d7765c5f6a465bfb5e49d5b2a43a8562e8f6
rnormak@tlu.ee <rnormak@tlu.ee@e2c1bf8b-2388-1911-6c94-c79eb88be6e8>
2014-01-07 21:01:46 +0000

    admin.php valmimas

--- trunk/taxi2/login.php
+++ trunk/taxi2/login.php
@@ -35,10 +35,10 @@
       Tere, <?php echo $_SESSION["roll"]." ".$_SESSION["kasnimi"]; ?>
       <a href="?lahku=jah">lahku</a>
       <ul>
-        <?php if($_SESSION["roll"]=="dispetser"): ?>
-		<li><a href="toovastu.php">Vastuvõtmine</a></li>
+        <?php if($_SESSION["roll"]=="haldur"): ?>
+		<li><a href="admin.php">Vastuvõtmine</a></li>
 		 <?php endif ?>
-        <?php if($_SESSION["roll"]=="tehnik"): ?>
+        <?php if($_SESSION["roll"]=="tehnika"): ?>
           <li><a href="tehnik.php">Pooleli olevad tööd</a></li>  
         <?php endif ?>
 		<?php if($_SESSION["roll"]=="haldur"): ?>
