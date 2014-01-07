dcda2bf2c41087059d20132ce5008ed55ac28af0
trege@tlu.ee <trege@tlu.ee@e2c1bf8b-2388-1911-6c94-c79eb88be6e8>
2014-01-07 22:44:13 +0000

    kella viiene tee

--- trunk/taxi2/user.php
+++ trunk/taxi2/user.php
@@ -7,9 +7,9 @@ session_start();
  require_once("konf.php");
  if(isSet($_REQUEST["sisestusnupp"])){
  $kask=$yhendus->prepare(
- "INSERT INTO tellimused(kasutajanimi, algpunkt, lopp_punkt, tel_nr, kinnitatud, valmis) VALUES (?, )");
+ "INSERT INTO tellimus(kasutajanimi, algpunkt, lopp_punkt, tel_nr) VALUES (?,?,?,?)");
  
-$kask->bind_param("s", $_REQUEST["kasutajanimi"], $_REQUEST["algpunkt"], $_REQUEST["sihtpunkt"], $_REQUEST["telefon"]);
+$kask->bind_param("ssss", $_SESSION["kasnimi"], $_REQUEST["algpunkt"], $_REQUEST["sihtpunkt"], $_REQUEST["telefon"]);
 $kask->execute();
 $yhendus->close();
 exit();
@@ -22,6 +22,7 @@ exit();
  <title>Tellimine</title>
  </head>
  <body>
+ Sisse logitud <?php echo $_SESSION["roll"]." ".$_SESSION["kasnimi"]; ?>
  <h1>Takso tellimine</h1>
  <?php
  if(isSet($_REQUEST["id"])){
@@ -30,12 +31,12 @@ exit();
 <form action="?">
  <dl>
  <dt>Sisesta tellimus:</dt>
-<dd><input type="text" name="kasutajanimi" /></dd>
-<dd><input type="text" name="algpunkt" /></dd>
-<dd><input type="text" name="sihtpunkt" /></dd>
-<dd><input type="text" name="telefon" /></dd>
+<dd>Alguspunt<input type="text" name="algpunkt" /></dd>
+<dd>Sihtpunkt<input type="text" name="sihtpunkt" /></dd>
+<dd>Telefoni nr.<input type="text" name="telefon" /></dd>
  <dt><input type="submit" name="sisestusnupp" value="sisesta" /></dt>
  </dl>
 </form>
+
 </body>
 </html>
