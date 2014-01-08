43887bed2c368d50e9bc44d7510f98b3d9b38b86
rnormak <rnormak@tlu.ee>
2014-01-08 01:15:08 +0000

    finishing touches

--- taxi2/admin.php
+++ taxi2/admin.php
@@ -36,7 +36,7 @@ $kask->execute();
 </head>
 <body>
 Sisse logitud <?php echo $_SESSION["roll"]." ".$_SESSION["kasnimi"]; ?>
-       <a href="index.php">avaleht</a>
+       <a href="index.php">Avaleht</a>
 <h1>Administraatori leht</h1>
 <table>
 <?php
